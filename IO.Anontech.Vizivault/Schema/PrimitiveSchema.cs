27a31ad128829574ac164f234238d0627d9f8d2b
Peter Wood <peter.wood@anontechnology.com>
2020-12-02 16:17:04 -0500

    more docstrings

--- IO.Anontech.Vizivault/Schema/PrimitiveSchema.cs
+++ IO.Anontech.Vizivault/Schema/PrimitiveSchema.cs
@@ -1,5 +1,8 @@
 namespace IO.Anontech.Vizivault.Schema {
 
+  /// <summary>
+  /// An attribute schema that is used to indicate that an attribute's value should be a single, unstructured piece of data.
+  /// </summary>
   public enum PrimitiveSchema {
     String, Integer, Boolean, Float, File, Date
   }
--- IO.Anontech.Vizivault/Tag.cs
+++ IO.Anontech.Vizivault/Tag.cs
@@ -1,9 +1,15 @@
 using System;
-using System.Text.Json.Serialization;
 
 namespace IO.Anontech.Vizivault {
 
+  /// <summary>
+  /// A tag that can be used to create arbitrary groupings of data.
+  /// </summary>
   public class Tag {
+
+    /// <summary>
+    /// A human-readable string that uniquely identifies this tag.
+    /// </summary>
     public string Name {get; set;}
 
     public DateTime CreatedDate {get; set;}
